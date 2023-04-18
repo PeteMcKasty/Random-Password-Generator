9e66f621ddba0e7f4172acaf0ffe0fbd3d273925
Peter McKasty <pmck25@yahoo.com>
2023-04-18 18:29:54 -0400

    added password criteria and alert when criteria are unselected

--- script.js
+++ script.js
@@ -7,8 +7,23 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  getParameters()
 
 }
+function getParameters() {
+  var useLowercase = confirm("Do you want to include lowercase characters?");
+  var useUppercase = confirm("Do you want to include uppercase characters?");
+  var useNumeric = confirm("Do you want to include numeric characters?");
+  var useSpecial = confirm("Do you want to include special characters?");
+}
+
+  while (!useLowercase && !useUppercase && !useNumeric && !useSpecial) {
+    alert("Please select at least one character type for your password.");
+    useLowercase = confirm("Do you want to include lowercase characters?");
+    useUppercase = confirm("Do you want to include uppercase characters?");
+    useNumeric = confirm("Do you want to include numeric characters?");
+    useSpecial = confirm("Do you want to include special characters?");
+  }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
